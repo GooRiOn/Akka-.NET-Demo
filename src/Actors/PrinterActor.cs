e6cae296a46c6a4d9c28bfe343ce34b0b538ec1d
GooRiOn <foreverframenet@gmail.com>
2020-12-28 18:52:45 +0100

    Added test project

---
+++ src/Actors/PrinterActor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Akka.Actor;
+
+namespace AkkaDemo.Actors
+{
+    public class PrinterActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            if (message is not null and string)
+            {
+                Console.WriteLine(message);
+                Sender.Tell(null);
+                return;
+            }
+            
+            Sender.Tell("nice");
+        }
+
+        public string GetPrintMessage()
+            => "Hello!";
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using AkkaDemo.Actors;
+
+namespace AkkaDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var system = ActorSystem.Create("System");
+
+            var printerActor = system.ActorOf<PrinterActor>("printer");
+            
+            var response = await printerActor.Ask("Hello world!");
+            
+            var response1 = await printerActor.Ask(new object());
+        }
+    }
+}
